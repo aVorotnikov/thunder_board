24e9850e09e5dd7d488214505151bb4be233d8a0
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-11-18 00:55:17 +0300

    Связь проект-задача убрана в скрипте создания базы данных

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -56,10 +56,8 @@ CREATE TABLE IF NOT EXISTS Task (
     taskDescription TEXT NOT NULL,
     taskTimeEstimation INTEGER NOT NULL,
     taskTimeSpent INTEGER NOT NULL,
-    projectId INTEGER NOT NULL,
     userId INTEGER NOT NULL,
     statusId INTEGER NOT NULL,
-    FOREIGN KEY(projectId) REFERENCES Project(projectId),
     FOREIGN KEY(userId) REFERENCES User(userId),
     FOREIGN KEY(statusId) REFERENCES Status(statusId)
 );
