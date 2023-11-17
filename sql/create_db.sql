3326c17ac8a2b918ab421cab97c573e44087fd1f
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-11-18 00:56:00 +0300

    У остатка времени введено значение по умолчанию

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS Task (
     taskName TEXT NOT NULL,
     taskDescription TEXT NOT NULL,
     taskTimeEstimation INTEGER NOT NULL,
-    taskTimeSpent INTEGER NOT NULL,
+    taskTimeSpent INTEGER DEFAULT 0 NOT NULL,
     userId INTEGER NOT NULL,
     statusId INTEGER NOT NULL,
     FOREIGN KEY(userId) REFERENCES User(userId),
