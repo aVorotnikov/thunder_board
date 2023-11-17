f470a61fdea3776baf0b34c3d2adeade63179e41
Andrey Vorotnikov <39947159+aVorotnikov@users.noreply.github.com>
2023-11-18 01:19:19 +0300

    Вставка задач

--- sql/test_data/insert.sql
+++ sql/test_data/insert.sql
@@ -39,55 +39,168 @@ INSERT OR IGNORE INTO UserProject (userId, projectId, roleId)
     WHERE userEmail="vitaliev@org.org" AND projectName="Альфа" AND roleName="manager";
 
 -- Insert Alpha statuses
-CREATE TEMP TABLE _DataToInsertInStatusNumber(
+CREATE TEMP TABLE _DataToInsertInStatus(
     statusName TEXT NOT NULL,
     statusNumber INTEGER NOT NULL
 );
-INSERT INTO _DataToInsertInStatusNumber (statusName, statusNumber) VALUES
+INSERT INTO _DataToInsertInStatus (statusName, statusNumber) VALUES
     ("Назначено", 0);
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Альфа" AND statusTypeName="initial";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Активно", statusNumber = 1;
+UPDATE _DataToInsertInStatus SET statusName = "Активно", statusNumber = 1;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Альфа" AND statusTypeName="intermediate";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Разрешено", statusNumber = 2;
+UPDATE _DataToInsertInStatus SET statusName = "Разрешено", statusNumber = 2;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Альфа" AND statusTypeName="intermediate";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Завершено", statusNumber = 3;
+UPDATE _DataToInsertInStatus SET statusName = "Завершено", statusNumber = 3;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Альфа" AND statusTypeName="final";
 
 -- Insert Beta statuses
-UPDATE _DataToInsertInStatusNumber SET statusName = "Поступило", statusNumber = 0;
+UPDATE _DataToInsertInStatus SET statusName = "Поступило", statusNumber = 0;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Бета" AND statusTypeName="initial";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Исследование", statusNumber = 1;
+UPDATE _DataToInsertInStatus SET statusName = "Исследование", statusNumber = 1;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Бета" AND statusTypeName="intermediate";
-UPDATE _DataToInsertInStatusNumber SET statusName = "В работе", statusNumber = 2;
+UPDATE _DataToInsertInStatus SET statusName = "В работе", statusNumber = 2;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Бета" AND statusTypeName="intermediate";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Завершено", statusNumber = 3;
+UPDATE _DataToInsertInStatus SET statusName = "Завершено", statusNumber = 3;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Бета" AND statusTypeName="final";
-UPDATE _DataToInsertInStatusNumber SET statusName = "Отклонено", statusNumber = 4;
+UPDATE _DataToInsertInStatus SET statusName = "Отклонено", statusNumber = 4;
 INSERT OR IGNORE INTO Status (statusName, statusNumber, projectId, statusTypeId)
     SELECT statusName, statusNumber, projectId, statusTypeId
-    FROM Project, StatusType, _DataToInsertInStatusNumber
+    FROM Project, StatusType, _DataToInsertInStatus
     WHERE projectName="Бета" AND statusTypeName="final";
+DROP TABLE _DataToInsertInStatus;
+
+-- Insert Alpha tasks
+CREATE TEMP TABLE _DataToInsertInTask(
+    taskName TEXT NOT NULL,
+    taskDescription TEXT NOT NULL,
+    taskTimeEstimation INTEGER NOT NULL,
+    taskTimeSpent INTEGER NOT NULL
+);
+INSERT INTO _DataToInsertInTask (taskName, taskDescription, taskTimeEstimation, taskTimeSpent) VALUES
+    (
+        "Добавить в команду Сергеева",
+        "Добавить в исполнители проекта Сергеева С.С. (sergeev@org.org)",
+        2 * 60,
+        0
+    );
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Альфа" AND userEmail="vitaliev@org.org" AND statusName="Назначено";
+UPDATE _DataToInsertInTask SET
+    taskName = "Создание новых задач",
+    taskDescription = 'Создавать задачи в проекте "Альфа"',
+    taskTimeEstimation = 200 * 60,
+    taskTimeSpent = 100 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Альфа" AND userEmail="petrov@org.org" AND statusName="Активно";
+UPDATE _DataToInsertInTask SET
+    taskName = "Шпионаж",
+    taskDescription = "Выяcнить планы проекта Бета",
+    taskTimeEstimation = 10 * 60,
+    taskTimeSpent = 100 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Альфа" AND userEmail="ivanov@org.org" AND statusName="Разрешено";
+UPDATE _DataToInsertInTask SET
+    taskName = "Полезная работа",
+    taskDescription = "Сделать хоть что-то полезное для проекта",
+    taskTimeEstimation = 1 * 60,
+    taskTimeSpent = 0;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Альфа" AND userEmail="vitaliev@org.org" AND statusName="Завершено";
+
+-- Insert Beta tasks
+UPDATE _DataToInsertInTask SET
+    taskName = "Сделать работу за проект Альфа",
+    taskDescription = 'От команды проекта "Альфа" поступил запрос на выполнение их работы',
+    taskTimeEstimation = 20 * 60,
+    taskTimeSpent = 0;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Бета" AND userEmail="ivanov@org.org" AND statusName="Поступило";
+UPDATE _DataToInsertInTask SET
+    taskName = "Антишпионаж",
+    taskDescription = 'Выявить шпиона команды "Альфа"',
+    taskTimeEstimation = 50 * 60,
+    taskTimeSpent = 20 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Бета" AND userEmail="petrov@org.org" AND statusName="Исследование";
+UPDATE _DataToInsertInTask SET
+    taskName = "Наблюдение за солнцем",
+    taskDescription = "Наблюдать за солнцем в телескоп",
+    taskTimeEstimation = 100 * 60,
+    taskTimeSpent = 50 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Бета" AND userEmail="ivanov@org.org" AND statusName="В работе";
+UPDATE _DataToInsertInTask SET
+    taskName = "Бесполезная работа",
+    taskDescription = "Сделать бесполезную работу",
+    taskTimeEstimation = 500 * 60,
+    taskTimeSpent = 1000 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Бета" AND userEmail="petrov@org.org" AND statusName="Завершено";
+UPDATE _DataToInsertInTask SET
+    taskName = "Полезная работа",
+    taskDescription = "Сделать полезную работу",
+    taskTimeEstimation = 2 * 60,
+    taskTimeSpent = 2 * 60;
+INSERT OR IGNORE INTO Task (taskName, taskDescription,
+    taskTimeEstimation, taskTimeSpent, userId, statusId)
+    SELECT taskName, taskDescription, taskTimeEstimation, taskTimeSpent, userId, statusId
+    FROM _DataToInsertInTask, User, Status
+    LEFT JOIN Project ON Status.projectId = Project.projectId
+    WHERE projectName="Бета" AND userEmail="ivanov@org.org" AND statusName="Отклонено";
+DROP TABLE _DataToInsertInTask;
