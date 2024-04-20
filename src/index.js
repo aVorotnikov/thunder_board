c7de1a581ca1217f0d681f12ab2418b72c1a9f0a
Ilya Pavlov <55149826+IlyaP01@users.noreply.github.com>
2024-04-20 19:44:29 +0300

    Страницы проектов и профиля и переадресация на логин

--- src/index.js
+++ src/index.js
@@ -4,29 +4,45 @@ import './index.css';
 import BoardPage from './pages/BoardPage';
 import ProjectsPage from './pages/ProjectsPage';
 import ProfilePage from './pages/ProfilePage';
+import LoginForm from './pages/Login';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-const router = createBrowserRouter([
-  {
-    path: "board/:projectId",
-    element: <BoardPage />
-  },
-  {
-    path: "projects/",
-    element: <ProjectsPage />
-  },
-  {
-    path: "profile/",
-    element: <ProfilePage />
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route
+        path="login"
+        element={<LoginForm />}
+      />
+      <Route
+        path="board/:projectId"
+        element={
+          <PrivateRoute>
+            <BoardPage />
+          </PrivateRoute>}
+      />
+      <Route
+        path="projects"
+        element={
+          <PrivateRoute>
+            <ProjectsPage />
+          </PrivateRoute>}
+      />
+      <Route
+        path="profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>}
+      />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div>
+        Login form
+    </div>
+  );
+}
+
+export default LoginForm;
