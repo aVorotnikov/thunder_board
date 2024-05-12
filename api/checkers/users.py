8288732d5ca921c84a3b85df427d35529403e2c3
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2024-05-12 21:18:21 +0300

    Функции проверки для пользователей

---
+++ api/checkers/users.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import db
+from user_role import UserRole
+
+
+def user_is_admin(user_id):
+    return db.get_user_info(user_id)[3]
+
+
+def get_user_role(user_id, project_id):
+    role = db.get_user_role(user_id, project_id)
+    if role is None:
+        return UserRole.No
+    return UserRole(role[0]).name
+
+
+def get_task_owner(task_id):
+    return db.get_task_owner(task_id)[0]
