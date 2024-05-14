f7b70440fef9a9e06fcb375e9420b51cc89f7039
Ilya Pavlov <55149826+IlyaP01@users.noreply.github.com>
2024-05-15 01:17:08 +0300

    Add route for root

--- src/index.js
+++ src/index.js
@@ -6,15 +6,31 @@ import ProjectsPage from './pages/ProjectsPage';
 import ProfilePage from './pages/ProfilePage';
 import LoginPage from './pages/LoginPage';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, useNavigate } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+function RedirectToProjects()
+{
+  const navigate = useNavigate();
+  React.useEffect(_ => {
+    navigate('/projects')
+  })
+  return <div></div>
+}
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <RedirectToProjects />
+            </PrivateRoute>}
+      />
       <Route
         path="login"
         element={<LoginPage />}
