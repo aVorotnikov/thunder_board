f3297efa9889773a0f5787c8731ce61a17e1ad77
Ilya Pavlov <55149826+IlyaP01@users.noreply.github.com>
2024-05-16 01:19:45 +0300

    Add authorization

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,10 @@
+import { Navigate } from "react-router-dom"
 import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
+  if (localStorage.getItem('loggedIn')) {
+    return <Navigate to="/" replace />;
+  }
   return (
     <div className="d-flex align-items-center justify-content-center vh-100">
       <LoginForm />
