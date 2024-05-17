cabb49ca18961af9eb6c53cf6efc251c54f4f2f1
Ilya Pavlov <55149826+IlyaP01@users.noreply.github.com>
2024-05-17 21:33:52 +0300

    Add filter to projects

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -6,8 +6,11 @@ import ProjectsTable from "../components/ProjectsTable";
 import { PaginationControl } from "react-bootstrap-pagination-control";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState(null)
   const [page, setPage] = useState(1)
+  const [users, setUsers] = useState(null)
+  const [usersFilter, setUsersFilter] = useState([])
+  const [textFilter, setTextFilter] = useState('')
 
   const navigate = useNavigate()
 
@@ -51,11 +54,58 @@ function ProjectsPage() {
     getProjects(1)
   }, [])
 
+  let getUsers = () => {
+    fetch('http://localhost:3000/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      },
+    })
+    .then((response) => {
+      if (response.ok)
+      {
+        return response.json()
+      }
+      else if (response.status === 401)
+      {
+        localStorage.setItem('loggedIn', false)
+        throw '/login'
+      }
+      else
+      {
+        throw '/error'
+      }
+    })
+    .then((json) => {
+      setUsers(json.users)
+    })
+    .catch((path) => {
+      navigate(path)
+    })
+  }
+
+  useEffect(() => {
+    getProjects(1)
+    getUsers()
+  }, [])
+
+  let filteredProjects = () => {
+    return projects.filter((project) => (project.name.toLowerCase().includes(textFilter.toLowerCase()) ||
+                                         project.description.toLowerCase().includes(textFilter.toLowerCase())) &&
+                                        (usersFilter.length == 0 || usersFilter.includes(project.user.name) )
+                       )
+  }
+
+  if (!projects || !users) {
+    return null
+  }
+
   return (
     <div>
       <NavBar />
-      <ProjectsFilter />
-      <ProjectsTable projects={projects} />
+      <ProjectsFilter users={users} usersFilterHandler={setUsersFilter} textFilterHandler={setTextFilter} />
+      <ProjectsTable projects={filteredProjects()} />
       <PaginationControl
         page={page}
         between={2}
